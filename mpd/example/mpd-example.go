a54b69568d8a18fad4a5042f587c18d638c76c5c
wberg <wberg@wberg.com>
2020-03-14 22:06:40 +0100

    penis

--- mpd/example/mpd-example.go
+++ mpd/example/mpd-example.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"gitlab.com/anderstorpsfestivalen/benis-phone/mpd"
 )
 
 func main() {
 	host := "[::1]:6600"
 	m := mpd.Init(host)
+	fmt.Println(m)
+
+	m.Add("output.mp3")
+	m.Next()
+	//m.PlaylistClear()
 }
--- mpd/mpd.go
+++ mpd/mpd.go
@@ -1,6 +1,7 @@
 package mpd
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fhs/gompd/mpd"
@@ -10,11 +11,11 @@ type MpdClient struct {
 	Host string
 }
 
-var c MpdClient
-
-func Init(h string) {
+func Init(h string) MpdClient {
+	var c MpdClient
 	c.Host = h
-	c.playlistClear()
+	c.PlaylistClear()
+	return c
 }
 
 func (c MpdClient) Add(f string) {
@@ -24,8 +25,9 @@ func (c MpdClient) Add(f string) {
 	}
 	defer conn.Close()
 
-	log.Printf("Adding %s\n", f)
+	conn.Update(f)
 
+	fmt.Println("Adding %s\n", f)
 	conn.Add(f)
 }
 
@@ -36,20 +38,21 @@ func (c MpdClient) Next() {
 	}
 	defer conn.Close()
 
-	log.Printf("Playging next in playlist\n")
+	fmt.Println("Playging next in playlist\n")
 	conn.Next()
 }
 
-func (c MpdClient) playlistClear() {
+func (c MpdClient) PlaylistClear() {
 	conn, err := mpd.Dial("tcp", c.Host)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer conn.Close()
 
-	log.Printf("Clearing playlist\n")
+	fmt.Println("Clearing playlist\n")
 
-	err = conn.PlaylistClear("test")
+	//err = conn.PlaylistClear("default")
+	err = conn.Clear()
 	if err != nil {
 		log.Fatalln(err)
 	}
