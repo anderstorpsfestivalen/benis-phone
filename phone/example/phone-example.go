710980c8b2110251b41e878a960df0829f80bf89
wberg <alex.wagberg@gmail.com>
2020-03-15 16:24:49 +0100

    snase

---
+++ phone/example/phone-example.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"gitlab.com/anderstorpsfestivalen/benis-phone/phone"
+)
+
+func main() {
+
+	state := phone.State()
+	fmt.Println(state)
+
+}
---
+++ phone/phone.go
@@ -0,0 +1,40 @@
+package phone
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+type Phone struct {
+	Pin_GPIO      string
+	handset_state bool
+}
+
+func Init_GPIO() {
+	var p *Phone
+	p.Pin_GPIO = rpio.Pin(6)
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func Close() {
+	defer rpio.Close()
+}
+
+func State() bool {
+	Init_GPIO()
+	var p *Phone
+	p.Pin_GPIO.Input()
+	res := p.Pin_GPIO.Read()
+	if res == 1 {
+		p.handset_state = true
+	}
+	if res == 0 {
+		p.handset_state = false
+	}
+	return p.handset_state
+}
