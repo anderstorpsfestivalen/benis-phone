b353f1fd502a229ecfa76fd9d37528ce2174300b
wberg <alex.wagberg@gmail.com>
2020-03-15 15:30:08 +0100

    röv

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module gitlab.com/anderstorpsfestivalen/benis-phone
 go 1.13
 
 require (
+	github.com/bmizerany/aws4 v0.0.0-20141025110357-5fb2e7239626 // indirect
 	github.com/fhs/gompd v2.0.0+incompatible
+	github.com/leprosus/golang-tts v0.0.0-20190516104601-a4737dbd315b
 	github.com/stianeikeland/go-rpio/v4 v4.4.0
 )
--- polly/polly.go
+++ polly/polly.go
@@ -1,22 +1,38 @@
-package main
+package polly
 
 import (
 	"io/ioutil"
+	"os"
+	"path"
 
 	golang_tts "github.com/leprosus/golang-tts"
 )
 
-func main() {
-	polly := golang_tts.New("aws_key", "aws_secret")
+type AWS struct {
+	aws_key    string
+	aws_secret string
+}
+
+func TTS(message string, voice string) string {
+	var a AWS
+
+	// Read temp env variable for key
+	a.aws_key = os.Getenv("aws_key")
+	a.aws_secret = os.Getenv("aws_secret")
+
+	polly := golang_tts.New(a.aws_key, a.aws_secret)
 
 	polly.Format(golang_tts.MP3)
 	polly.Voice(golang_tts.Astrid)
 
-	text := "Hej hej, jag provar Astrid på Svenska"
-	bytes, err := polly.Speech(text)
+	bytes, err := polly.Speech(message)
 	if err != nil {
 		panic(err)
 	}
 
-	ioutil.WriteFile("./output.mp3", bytes, 0644)
+	home, err := os.UserHomeDir()
+	filename := path.Join(home + "/Music/test.mp3")
+	ioutil.WriteFile(filename, bytes, 0644)
+
+	return filename
 }
