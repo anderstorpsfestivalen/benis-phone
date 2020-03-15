9b4101474e82fe79da2a74214948cbe40a315cd8
coral <jonas@jonasbengtson.se>
2020-03-15 13:02:40 -0700

    JIMMY LÖK

--- phone/example/phone-example.go
+++ phone/example/phone-example.go
@@ -10,9 +10,14 @@ func main() {
 
 	ph := phone.Init(6)
 
+	fmt.Println(ph.State())
 	for {
-		s := <-ph.HookChannel
-		fmt.Println(s)
+		select {
+		case s := <-ph.HookChannel:
+			fmt.Println(s)
+		default:
+			fmt.Println("BEEEEEEEENIS")
+		}
 	}
 
 }
