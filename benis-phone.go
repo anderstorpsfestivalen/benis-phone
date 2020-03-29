efef53a38fd06ea1ece18131fe94cf15c68e85e9
coral <jonas@jonasbengtson.se>
2020-03-29 12:26:15 -0700

    kek

--- benis-phone.go
+++ benis-phone.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"gitlab.com/anderstorpsfestivalen/benis-phone/controller"
 	"gitlab.com/anderstorpsfestivalen/benis-phone/dtmf"
 	"gitlab.com/anderstorpsfestivalen/benis-phone/mpd"
 	"gitlab.com/anderstorpsfestivalen/benis-phone/phone"
-	"gitlab.com/anderstorpsfestivalen/benis-phone/polly"
 )
 
 func main() {
@@ -16,28 +13,8 @@ func main() {
 	ph := phone.Init(6)
 	mpd := mpd.Init("127.0.0.1:6600")
 
-	ctrl := controller.New(ph, mpd)
+	ctrl := controller.New(ph, mpd, dtmf)
 
-	fmt.Println(ctrl)
-	//ctrl.Start()
-
-	dtmf_key := <-dtmf.HookKey
-
-	switch dtmf_key {
-	case "1":
-		message := "orvars korvar och makaroner"
-		polly.TTS(message, "Astrid")
-		fmt.Println(dtmf_key, message)
-
-		mpd.Add("test.mp3")
-		mpd.Play()
-	case "2":
-		message := "penis lasse"
-		polly.TTS(message, "Astrid")
-		fmt.Println(dtmf_key, message)
-
-		mpd.Add("test.mp3")
-		mpd.Play()
-	}
+	ctrl.Start()
 
 }
--- controller/controller.go
+++ controller/controller.go
@@ -1,19 +1,26 @@
 package controller
 
 import (
+	"fmt"
+
+	"gitlab.com/anderstorpsfestivalen/benis-phone/dtmf"
+
 	"gitlab.com/anderstorpsfestivalen/benis-phone/mpd"
 	"gitlab.com/anderstorpsfestivalen/benis-phone/phone"
+	"gitlab.com/anderstorpsfestivalen/benis-phone/polly"
 )
 
 type Controller struct {
 	phone phone.Phone
 	mpd   mpd.MpdClient
+	dtmf  dtmf.Dtmf
 }
 
-func New(ph phone.Phone, mpd mpd.MpdClient) Controller {
+func New(ph phone.Phone, mpd mpd.MpdClient, dtmf dtmf.Dtmf) Controller {
 	return Controller{
 		phone: ph,
 		mpd:   mpd,
+		dtmf:  dtmf,
 	}
 }
 
@@ -22,10 +29,32 @@ func (c *Controller) Start() {
 	for {
 		s := <-c.phone.HookChannel
 		if s {
-			//c.mpd.Add("test.mp3")
-			c.mpd.Play()
+			select {
+			case dtmf_key := <-c.dtmf.HookKey:
+				c.MainMenu(dtmf_key)
+			}
 		} else {
 			c.mpd.Clear()
 		}
+	}
+}
+
+func (c *Controller) MainMenu(dtmf_key string) {
+
+	switch dtmf_key {
+	case "1":
+		message := "orvars korvar och makaroner"
+		polly.TTS(message, "Astrid")
+		fmt.Println(dtmf_key, message)
+
+		c.mpd.Add("test.mp3")
+		c.mpd.Play()
+	case "2":
+		message := "penis lasse"
+		polly.TTS(message, "Astrid")
+		fmt.Println(dtmf_key, message)
+
+		c.mpd.Add("test.mp3")
+		c.mpd.Play()
 	}
 }
