b0e9d1ca2684ceea434516113af1a31abf4b453f
coral <jonas@jonasbengtson.se>
2020-10-04 13:56:49 -0700

    barskåpÄ

--- controller/controller.go
+++ controller/controller.go
@@ -64,7 +64,7 @@ func (c *Controller) Start(wg *sync.WaitGroup) {
 					log.Info("Hook is lifted")
 					tm := time.Now()
 					recTime := tm.Format("2006-01-02_15:04:05")
-					err := c.Recorder.Record(recTime)
+					err := c.Recorder.Record("random/" + recTime)
 					if err != nil {
 						log.Error(err)
 					}
