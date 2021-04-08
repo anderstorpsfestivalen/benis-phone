2fa4efd9a773d386724bb1532b89a823be9b6e76
wberg <alex.wagberg@gmail.com>
2021-04-08 20:37:44 +0200

    jameson cola

---
+++ controller/fulolpoints.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"fmt"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+	"gitlab.com/anderstorpsfestivalen/benis-phone/services/backend"
+)
+
+type FulolPoints struct {
+}
+
+func (m *FulolPoints) Run(c *Controller, k string, menu MenuReturn) MenuReturn {
+	fulolpoints, err := backend.GetFulolPointsForPhoneNumber(k)
+	_ = fulolpoints
+	if err != nil {
+		ttsData, err := c.Polly.TTS("Telefonnummret kan inte hittas, var god försök igen.", "Astrid")
+		if err != nil {
+			log.Error(err)
+		}
+		c.Audio.PlayMP3FromStream(ttsData)
+
+		return MenuReturn{
+			NextFunction: menu.Caller,
+		}
+	}
+
+	message := fulolpoints.Name + ". Din fulöls poäng är: " +
+		strconv.FormatFloat(fulolpoints.Points, 'f', 0, 64) +
+		". Poäng."
+
+	fmt.Println(message)
+
+	ttsData, err := c.Polly.TTS(message, "Astrid")
+	if err != nil {
+		return MenuReturn{
+			Error:        err,
+			NextFunction: "error",
+		}
+	}
+	c.Audio.PlayMP3FromStream(ttsData)
+
+	return MenuReturn{
+		NextFunction: menu.Caller,
+	}
+
+}
+
+func (m *FulolPoints) InputLength() int {
+	return 10
+}
+
+func (m *FulolPoints) Name() string {
+	return "fulolpoints"
+}
+
+func (m *FulolPoints) Prefix(c *Controller) {
+	message := "Fyll i ditt telefonnummer, tio siffror."
+	ttsData, err := c.Polly.TTS(message, "Astrid")
+	if err != nil {
+		log.Error(err)
+	}
+
+	go c.Audio.PlayMP3FromStream(ttsData)
+}
---
+++ services/backend/fulolpoints.go
@@ -0,0 +1,63 @@
+package backend
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"gitlab.com/anderstorpsfestivalen/benis-phone/pkg/secrets"
+)
+
+type FulolPointsResp struct {
+	Points  float64 `json:"Points"`
+	Name    string  `json:"Name"`
+	Message string  `json:"Message"`
+}
+
+func GetFulolPointsForPhoneNumber(number string) (FulolPointsResp, error) {
+
+	br := FulolPointsResp{}
+
+	client := &http.Client{}
+	form := url.Values{}
+	form.Set("number", number)
+	fmt.Println("Inputted number is: " + number)
+
+	req, err := http.NewRequest("POST", "https://anderstorpsfestivalen.se/api/phone/fulolpoints", strings.NewReader(form.Encode()))
+	if err != nil {
+		return FulolPointsResp{}, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+
+	username := secrets.Loaded.Backend.Username
+	password := secrets.Loaded.Backend.Password
+
+	// Error check for missing credentials in creds.json
+	if secrets.Loaded.Backend.Username == "" || secrets.Loaded.Backend.Password == "" {
+		return FulolPointsResp{}, fmt.Errorf("No credentials for backend loaded.")
+	}
+
+	req.SetBasicAuth(username, password)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return FulolPointsResp{}, err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&br)
+	if err != nil {
+		return FulolPointsResp{}, err
+	}
+
+	if br.Message != "" {
+		return FulolPointsResp{}, fmt.Errorf(br.Message)
+	}
+
+	return br, nil
+
+}
