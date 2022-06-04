9878c2555e438c4d8f693be95ddb1eea157cbc7b
coral <jonas@jonasbengtson.se>
2022-06-04 12:14:07 -0700

    ok

---
+++ gates/gates.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"gitlab.com/anderstorpsfestivalen/benis-phone/services/drogslang"
+	"gitlab.com/anderstorpsfestivalen/benis-phone/services/flacornot"
+	"gitlab.com/anderstorpsfestivalen/benis-phone/services/idiom"
+	"gitlab.com/anderstorpsfestivalen/benis-phone/services/train"
+)
+
+type Gate interface {
+	Get(input string, template string, arguments map[string]string) (string, error)
+	MaxInputLength() int
+}
+
+type ServiceResponse struct {
+	Message string
+	State   map[string]string
+}
+
+var ServiceRegistry = map[string]Service{
+	"flacornot": &flacornot.FlacOrNot{},
+	// "barclosing": &barclosing.BarClosing{},
+	"idiom":      &idiom.Idiom{},
+	"traintimes": &train.Train{},
+	"drogslang":  &drogslang.Drogslang{},
+}
